10e19c6cee06101f91ba0cd1f2f019bbea9dbf54
Bruno Tomé <ibrunotome@gmail.com>
2017-12-12 14:34:27 -0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew cask
+brew tap caskroom/cask
+
+# Install apps
+brew cask install phpstorm && \
+brew cask install pycharm-ce && \
+brew cask install datagrip && 
+brew cask install visual-studio-code && \
+brew cask install clipy && \
+brew cask install docker && \
+brew cask install google-chrome && \
+brew cask install firefox && \
+brew cask install spotify && \
+brew cask install telegram && \
+brew cask install slack && \
+brew cask install whatsapp
