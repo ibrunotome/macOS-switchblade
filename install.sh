11dd1b2b941c253d7333677368c34ae9e99612d0
Bruno Tomé <ibrunotome@gmail.com>
2019-04-29 08:20:59 -0300

    Update with my 2019 tools stack

--- install.sh
+++ install.sh
@@ -1,34 +1,34 @@
+# Install command line tools
+xcode-select --install
+
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install php72-pcntl
+brew install zsh
+brew install go
 brew install node
-brew install mariadb
-brew services start mariadb
-brew install redis
-brew services start redis
+brew install yarn --without-node
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install brew cask
 brew tap caskroom/cask
 
 # Install apps with GUI
-brew cask install phpstorm
-brew cask install pycharm-ce
-brew cask install datagrip
-brew cask install visual-studio-code
+brew cask install brave-browser
 brew cask install clipy
 brew cask install docker
-brew cask install postman
-brew cask install google-chrome
+brew cask install dropbox
 brew cask install firefox
+brew cask install graphql-playground
+brew cask install google-chrome
+brew cask install google-cloud-sdk
+brew cask install iterm2
+brew cask install jetbrains-toolbox
+brew cask install postman
+brew cask install slack
 brew cask install spotify
 brew cask install telegram
-brew cask install slack
+brew cask install visual-studio-code
 brew cask install whatsapp
-
-# Install composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
